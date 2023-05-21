850b109dd6732398474b55095862aea2ad05aaba
rizalyala <rizalyala19@gmail.com>
2023-05-21 13:45:21 +0700

    Redesign

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-var contents = document.querySelectorAll(".content");
+// Mencegah zoom in atau zoom out
+function disableZoom() {
+    document.addEventListener('gesturestart', function (e) {
+        e.preventDefault();
+    });
 
-function checkContents() {
-    for (var i = 0; i < contents.length; i++) {
-        var content = contents[i];
-        var position = content.getBoundingClientRect();
-
-        // If the top of the element is in the viewport
-        if (position.top < window.innerHeight) {
-            content.classList.add("show");
+    document.addEventListener('keydown', function (e) {
+        if (e.ctrlKey || e.metaKey) {
+            e.preventDefault();
         }
-    }
+    });
 }
-
-// Check contents on page load
-checkContents();
-
-// Check contents on scroll
-window.addEventListener("scroll", function() {
-    checkContents();
-});
